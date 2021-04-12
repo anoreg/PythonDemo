9421dc19c2fba4ae013603a26e25e773b0b11bbf
anoreg <7417760+anoreg@users.noreply.github.com>
2021-04-12 19:01:17 +0800

    Add caesar function

--- src/appbrewery/caesar_cipher.py
+++ src/appbrewery/caesar_cipher.py
@@ -6,30 +6,64 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-# TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(msg: str, shift: int) -> str:
+def _encrypt(msg: str, shift: int) -> str:
+    # HINT: How do you get the index of an item in a list:
+    # https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+    # 🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
     cipher: str = ''
     for cr in msg:
         index: int = alphabet.index(cr) + shift
-        diff: int = (len(alphabet) - 1) - index
-        cipher += alphabet[index if diff >= 0 else abs(diff) - 1]  # index is zero based
+        cipher += alphabet[_get_encrypt_index(index)]
     print(f"The encoded text is {cipher}")
     return cipher
 
-    # TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards
-    #  in the alphabet by the shift amount and print the encrypted text.
-    # e.g.
-    # plain_text = "hello"
-    # shift = 5
-    # cipher_text = "mjqqt"
-    # print output: "The encoded text is mjqqt"
 
-    ##HINT: How do you get the index of an item in a list:
-    # https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+def _decrypt(msg: str, shift: int) -> str:
+    plain: str = ""
+    for cr in msg:
+        index: int = alphabet.index(cr) - shift
+        plain += alphabet[_get_decrypt_index(index)]
+    print(f"The decoded text is {plain}")
+    return plain
 
 
-# TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code
-#  and encrypt a message.
-encrypt(msg=text, shift=shift)
+global recursion
+recursion: int = 0
+
+
+def _get_encrypt_index(index: int) -> int:
+    global recursion
+    recursion += 1
+    diff: int = (len(alphabet) - 1) - index  # index is zero based
+    return index if diff >= 0 else _get_encrypt_index(abs(diff) - 1)
+
+
+def _get_decrypt_index(index: int) -> int:
+    return index if index >= 0 else _get_decrypt_index(len(alphabet) + index)
+
+
+def caesar(text: str, shift: int, direct: str) -> str:
+    result: str = ""
+    shift = shift % len(alphabet)
+    print(f'shift {shift}')
+    if direct == "decode":
+        shift *= -1
+    for letter in text:
+        if letter in alphabet:
+            index = alphabet.index(letter.strip())
+            new_index = index + shift
+            result += alphabet[_get_caesar_index(new_index, direct)]
+        else:
+            result += letter
+    global recursion
+    print(f'recur {recursion}')
+    recursion = 0
+    print(f'The {direct} text is {result}')
+    return result
+
+
+def _get_caesar_index(index: int, direct: str) -> int:
+    return _get_encrypt_index(index) if direct == 'encode' else _get_decrypt_index(index)
+
+
+caesar(text=text, shift=shift, direct=direction)
